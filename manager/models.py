d5b0e695f8067473011453c7b92745c085113330
sebah12 <sebah_12@hotmail.com>
2017-12-03 17:24:05 -0300

    Create Models for Marca and Item

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Marca(models.Model):
+    marca_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+
+
+class Item(models.Model):
+    item_id = models.IntegerField(primary_key=True)
+    descripcion = models.CharField(max_length=50, unique=True)
+    marca = models.ForeignKey(Marca, related_name='items')
