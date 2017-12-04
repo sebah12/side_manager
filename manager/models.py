fdab14826f9922b82418585a0026a560e1f15556
sebah12 <sebah_12@hotmail.com>
2017-12-03 22:15:20 -0300

    Make Marca return self.nombre

--- manager/models.py
+++ manager/models.py
@@ -7,6 +7,9 @@ class Marca(models.Model):
     marca_id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Item(models.Model):
     item_id = models.IntegerField(primary_key=True)
