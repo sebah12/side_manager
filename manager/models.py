1716fa03f2bbabbb1b11bd75ea929c04e9399fbb
sebah12 <sebah_12@hotmail.com>
2017-12-08 23:00:49 -0300

    Adding soft delete feature to models

--- manager/models.py
+++ manager/models.py
@@ -1,9 +1,57 @@
-from django.db import models
+from django.db import models, QuerySet
 from django.contrib.auth.models import User
+import timezone
+
+
+class SoftDeletionManager(models.Manager):
+    def __init__(self, *args, **kwargs):
+        self.alive_only = kwargs.pop('alive_only', True)
+        super(SoftDeletionManager, self).__init__(*args, **kwargs)
+
+    def get_queryset(self):
+        if self.alive_only:
+            return SoftDeletionQuerySet(self.model).filter(deleted_at=None)
+        return SoftDeletionQuerySet(self.model)
+
+    def hard_delete(self):
+        return self.get_queryset().hard_delete()
+
+
+class SoftDeletionQuerySet(QuerySet):
+    def delete(self):
+        return super(SoftDeletionQuerySet, self).update(deleted_at=timezone.now())
+
+    def hard_delete(self):
+        return super(SoftDeletionQuerySet, self).delete()
+
+    def alive(self):
+        return self.filter(deleted_at=None)
+
+    def dead(self):
+        return self.exclude(deleted_at=None)
+
+
+class SoftDeletionModel(models.Model):
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    objects = SoftDeletionManager()
+    all_objects = SoftDeletionManager(alive_only=False)
+
+    class Meta:
+        abstract = True
+
+    def delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def hard_delete(self):
+        super(SoftDeletionModel, self).delete()
 
 
 # Create your models here.
-class Marca(models.Model):
+
+
+class Marca(SoftDeletionModel):
     marca_id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
 
@@ -11,7 +59,7 @@ class Marca(models.Model):
         return self.nombre
 
 
-class Item(models.Model):
+class Item(SoftDeletionModel):
     item_id = models.IntegerField(primary_key=True)
     descripcion = models.CharField(max_length=50, unique=True)
     marca = models.ForeignKey(Marca, related_name='items')
