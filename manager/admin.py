9fb0bc362a8b6eb65aad52ad9b8f3bb22b64ce05
sebah12 <sebah_12@hotmail.com>
2017-12-14 15:41:14 -0300

    Add new models to admin

--- manager/admin.py
+++ manager/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Marca, Item
+from .models import Marca, Item, Remito, CampoRemito
 
 admin.site.register(Marca)
 admin.site.register(Item)
+admin.site.register(Remito)
+admin.site.register(CampoRemito)
