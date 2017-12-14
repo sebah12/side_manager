1affa3f17f8e2ad6abc59ee9a383dbf5702941bd
sebah12 <sebah_12@hotmail.com>
2017-12-13 22:32:33 -0300

    Add requirements.txt and implement dj_database

--- side_project/settings.py
+++ side_project/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from decouple import config
+from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['192.168.1.106', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -81,15 +82,9 @@ WSGI_APPLICATION = 'side_project.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'side_manager',
-        'USER': 'side',
-        'PASSWORD': 'sidelectricidad',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )}
 
 
 # Password validation
