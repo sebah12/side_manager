46106b0ef611ef959d5e489bf3e8be9fd4e88a8b
sebah12 <sebah_12@hotmail.com>
2017-12-20 20:36:36 -0300

    Add ItemLogs and Precio to Registered models in admin

--- manager/admin.py
+++ manager/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Marca, Item, Remito, CampoRemito
+from .models import Marca, Item, Remito, CampoRemito, ItemLogs, Precio
 
 admin.site.register(Marca)
 admin.site.register(Item)
 admin.site.register(Remito)
 admin.site.register(CampoRemito)
+admin.site.register(ItemLogs)
+admin.site.register(Precio)
